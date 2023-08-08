dec49055faaacdc73c0503f97be0d5f0547ae66b
leejisu <ejeesoo97@gmail.com>
2023-08-08 15:21:16 +0900

    함수형 프로그래밍 기본 원칙
    - 하나의 기능에 집중
    - 함수를 여러 개로 구성

--- src/main.js
+++ src/main.js
@@ -8,6 +8,5 @@
 // 웹 표준: 파일 확장자가 필요
 // Vite 환경: 파일 확장자 생략 가능 (.js, .jsx, .ts, .tsx, .json)
 
-
-import './styles/main.css';
-import './FP/chapters/00-programming-paradigm';
+import "./styles/main.css";
+import "./FP/chapters/01-functional-principle";
